c8cafd4605c119ed15ccf6ca1bb9480e90c2c7fc
rsignes <romain.signes@gmail.com>
2020-09-17 11:27:12 +0200

    race reducer delete actiobn dispatched

--- src/app/components/race-list/race-list.component.ts
+++ src/app/components/race-list/race-list.component.ts
@@ -1,7 +1,10 @@
+import { AppState } from './../../store/app.state';
 import { Observable } from 'rxjs';
 import { Race } from './../../interfaces/race.interface';
 import { DataService } from './../../services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { deleteRace } from 'src/app/store/actions/race.actions';
 
 @Component({
   selector: 'app-race-list',
@@ -12,10 +15,14 @@ export class RaceListComponent implements OnInit {
 
   races$: Observable<Race[]>
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.races$ = this.dataService.races
+    this.races$ = this.store.pipe(select('races'))
+  }
+
+  delete(id: string) {
+    this.store.dispatch(deleteRace({ id }))
   }
 
 }
